cc5a22126bc66f17a3e729e82b52c1a978fb9b20
Sakthi Priyan <sakthi.priyan@anthology.com>
2025-08-06 09:13:43 -0400

    added forOff.js file to repo

---
+++ forOff.js
@@ -0,0 +1,12 @@
+var arr = [1,2,3,4,5];
+
+function somefn(){
+    for(let i=0; i<=arr.length; i++){
+        console.log(arr[i]);
+    }
+
+    for(let arrData of arr){
+        console.log("2 x " + arrData +" = " +arrData*2);
+    }
+}
+somefn();
