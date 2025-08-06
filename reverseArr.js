6fec273943f6a574c622b48c05b35b617eca4f15
Sakthi Priyan <sakthi.priyan@anthology.com>
2025-08-06 10:37:22 -0400

    updates comments in reverseArr.js file

--- reverseArr.js
+++ reverseArr.js
@@ -1,10 +1,10 @@
 function revArr(){
-    let arr = ["s","a","k","t","h","i"];
-    let rev = [];
+    let arr = ["s","a","k","t","h","i"]; // initialize array.
+    let rev = []; // creating emply arr and named rev.
 
     for(let i=arr.length-1; i>=0; i--){
-        rev.push(arr[i]);
+        rev.push(arr[i]); // using .push array to empty arr rev[].
     }
-    return rev;
+    return rev; // return rev.
 }
-console.log(revArr());
+console.log(revArr()); //output ["i","h","t","k","a","s"]
