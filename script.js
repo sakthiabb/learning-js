c263bdb8e17239379fa2552bfdbcb5ac180171e2
Sakthi Priyan <sakthi.priyan@anthology.com>
2025-08-06 08:46:43 -0400

    for loop added

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var arr = [1,2,3,4,5];
 var arrstr = ["sakthi", "priyan"];
 
 function somefn(){
-    // for(let i=0; i<=arr.length; i++){
-    //     console.log(arr[i]);
-    // }
+    for(let i=0; i<=arr.length; i++){
+        console.log(arr[i]);
+    }
+    
     for(let arrData of arr){
         console.log("2 x " + arrData +" = " +arrData*2);
     }
